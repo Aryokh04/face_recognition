07d852271e6398bdc50ac926e068448da160d233
Aryokh04 <143172110+Aryokh04@users.noreply.github.com>
2025-06-09 00:15:16 +0200

    small changes

--- src/main.py
+++ src/main.py
@@ -5,18 +5,19 @@ face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fro
 # Creating a function that detects a face based on input image. Using OpenCV´s Haar Cascade algorithm for detecting objects from an image
 def detect_face(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    face = face_classifier.detectMultiScale(gray, 1.1, 5)
+    face = face_classifier.detectMultiScale(gray, 1.1, 7)
     
     # Checking if face is detected, and if so, create a rectangle around face when detecting
-    if len(faces) > 0:
+    if len(face) > 0:
         for (x, y, w, h) in face:
           cv2.rectangle(image, (x,y), (x + w, y + h), (255, 255, 0), 2)  # Creating the rectangle based on measures and choosing color and line thickness
         return image
     else:
         return image
     
-def capture():
-    cap = cv2.VideoCapture(0)  # Start camera capture
+
+def capture(): 
+    cap = cv2.VideoCapture(0) # Start camera capture
 
     while True:
         ret, frame = cap.read()
@@ -24,8 +25,11 @@ def capture():
 
         cv2.imshow('Face Detection', frame)
 
-        if cv2.waitkey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+   capture()
